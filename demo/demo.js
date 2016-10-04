d86c05ddd629ecb3b15f34f23eb574ccfcb305c1
Scukerman <nngoblin@gmail.com>
2016-10-04 17:16:43 +0300

    Refactoring and reformatting

--- demo/demo.js
+++ demo/demo.js
@@ -1,11 +1,11 @@
-(function(){
+(function() {
 	var demo = angular.module('demo', ['datetimePicker']);
-	demo.controller('Demo1Ctrl', function($scope){
+	demo.controller('Demo1Ctrl', function($scope) {
 		$scope.model = {
 			timestamp: 1402292961
 		};
 	});
-	demo.controller('Demo2Ctrl', function($scope){
+	demo.controller('Demo2Ctrl', function($scope) {
 		$scope.model = {
 			range: {
 				start: 1402292961 - 100000,
--- src/angular-datetimepicker.js
+++ src/angular-datetimepicker.js
@@ -1,21 +1,19 @@
-(function(){
-
+(function() {
 	var m = angular.module('datetimePicker', []);
 
-	m.directive('datetimePicker', function(){
-
-		Date.parseDate = function(input, format){
+	m.directive('datetimePicker', function() {
+		Date.parseDate = function(input, format) {
 			return moment(input, format).toDate();
 		};
 
-		Date.prototype.dateFormat = function(format){
+		Date.prototype.dateFormat = function(format) {
 			return moment(this).format(format);
 		};
 
 		return {
 			restrict: 'A',
 			require: '?ngModel',
-			link: function(scope, element, attrs, ngModel){
+			link: function(scope, element, attrs, ngModel) {
 
 				var format = attrs.unixtime || 'YYYY/MM/DD HH:mm',
 					formatDate = 'YYYY/MM/DD',
@@ -28,7 +26,7 @@
 				});
 
 				if(ngModel)
-					ngModel.$parsers.push(function(value){
+					ngModel.$parsers.push(function(value) {
 						if(typeof value === 'undefined') return value;
 						return ngModel.$isEmpty(value) ? null : value;
 					});
@@ -36,7 +34,7 @@
 		};
 	});
 
-	m.directive('datetimeRangePicker', function(){
+	m.directive('datetimeRangePicker', function() {
 		return {
 			restrict: 'E',
 			scope: {
@@ -44,34 +42,18 @@
 			},
 			template: '<input datetime-picker unixtime ng-model="datetimeRangePicker.start">&nbsp;-&nbsp;<input datetime-picker unixtime ng-model="datetimeRangePicker.end">' +
 				'&nbsp;&nbsp;<select ng-model="model.range" ng-options="interval.title for interval in intervals"></select>',
-			controller: ['$scope', function($scope){
+			controller: ['$scope', function($scope) {
 				$scope.intervals = [
-					{
-						title: 'Today',
-						start: moment().startOf('day').unix(),
-						end: moment().endOf('day').unix()
-					},
-					{
-						title: 'Yesterday',
-						start: moment().subtract('days', 1).startOf('day').unix(),
-						end: moment().subtract('days', 1).endOf('day').unix()
-					},
-					{
-						title: 'Last week',
-						start: moment().subtract('weeks', 1).startOf('day').unix(),
-						end: moment().unix()
-					},
-					{
-						title: 'Last month',
-						start: moment().subtract('months', 1).startOf('month').unix(),
-						end: moment().subtract('months', 1).endOf('month').unix()
-					}
+					{ title: 'Today', start: moment().startOf('day').unix(), end: moment().endOf('day').unix() },
+					{ title: 'Yesterday', start: moment().subtract('days', 1).startOf('day').unix(), end: moment().subtract('days', 1).endOf('day').unix() },
+					{ title: 'Last week', start: moment().subtract('weeks', 1).startOf('day').unix(), end: moment().unix() },
+					{ title: 'Last month', start: moment().subtract('months', 1).startOf('month').unix(), end: moment().subtract('months', 1).endOf('month').unix() }
 				];
-				$scope.$watch('model.range', function(range){
-					if(range && (range.start || range.end))
-					{
-						if(!$scope.datetimeRangePicker)
+				$scope.$watch('model.range', function(range) {
+					if(range && (range.start || range.end)) {
+						if(!$scope.datetimeRangePicker) {
 							$scope.datetimeRangePicker = {};
+						}
 						$scope.datetimeRangePicker.start = range.start;
 						$scope.datetimeRangePicker.end = range.end;
 					}
@@ -80,20 +62,19 @@
 		};
 	});
 
-	m.directive('unixtime', function(){
+	m.directive('unixtime', function() {
 		return {
 			require: '?ngModel',
 			restrict: 'A',
-			link: function(scope, element, attrs, ngModel){
-
+			link: function(scope, element, attrs, ngModel) {
 				var format = attrs.unixtime || 'YYYY/MM/DD HH:mm';
 
-				ngModel.$formatters.push(function(value){
+				ngModel.$formatters.push(function(value) {
 					if(!value) return value;
 					return moment.unix(value).format(format);
 				});
 
-				ngModel.$parsers.push(function(value){
+				ngModel.$parsers.push(function(value) {
 					if(!value) return value;
 					return moment(value, format).unix();
 				});
